f2eec2cf2a00a839f7b58943e117169d339ef0c5
Thouseef <thousithouseef731@gmail.com>
2024-02-26 11:59:05 +0530

    fix:carousel

--- belipark/urls.py
+++ belipark/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("", include("home.urls")),
     path("assets/",include("assets.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
