ff17b11abfdaffa70e3d5ac82b6a566119cf0863
thouseef-hamza <thousithouseef731@gmail.com>
2024-03-11 08:10:43 +0000

    commit after host:

--- belipark/urls.py
+++ belipark/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("assets/",include("assets.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
